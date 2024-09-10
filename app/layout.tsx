12ab36d9d36a37293e24f8dd9f3d31536d5abfc3
alsonwangkhem <alsonwangkhem20092@gmail.com>
2024-09-10 19:53:45 +0530

    complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
-import Header from "@/components/Header";
-import { ClerkLoaded, ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Flash",
+  description: "AI powered flashcard generator app. Upload a pdf and generate flashcards which are pairs of questions and answers in an instant.",
+};
 
 export default function RootLayout({
   children,
@@ -9,13 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-
-      <ClerkLoaded>
-          <div className="h-screen flex flex-col w-full">
-            <Header />
-            <main className={`flex-1 flex justify-center w-full ${inter.className}`}>{children}</main>
-          </div>
-      </ClerkLoaded>
-
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,16 @@
-import AllDocs from "@/components/AllDocs"
-import { Button } from "@/components/ui/button"
-import { FilePlus2 } from "lucide-react"
-import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
-function Dashboard() {
-  return (
-    <div className="flex flex-col">
-      <div className="flex justify-center m-5">
-              <Link href={"/dashboard/upload"}>
-                  <Button>
-                      <FilePlus2/>
-                      <p className="ml-2">Add your document</p>
-                  </Button>
-              </Link>
-      </div>
-      <div className="flex-1 drop-shadow-xl">
-        <AllDocs />
-      </div>
+export default function Home() {
+  return <div className="flex space-y-5 flex-col justify-center items-center h-screen bg-gradient-to-br from-slate-300 to-white">
+    <div className="flex flex-col space-y-5 justify-center items-center border rounded-xl shadow-2xl p-8 m-4">
+      <h1 className="text-3xl font-extrabold">Your favorite <span className="shadow-lg bg-slate-300">AI flashcard</span> generator</h1>
+      <p className="text-center text-xl">Upload your notes in PDF format. And get the questions you need to ace that exam in an instant.</p>
+      <p className="font-bold">PS: The contents of the PDF should be actual text and not photos.</p>
+      <Button asChild>
+        <Link href="/dashboard">Get Started</Link>
+      </Button>
     </div>
-  )
+  </div>;
 }
-
-export default Dashboard
